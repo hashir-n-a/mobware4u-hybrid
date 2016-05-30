eca4519efdcc6129b1d1ac9efa027df36c67b03a
Hashir N A <hashir@opencabs.com>
2016-05-30 06:00:22 +0530

    FIX : Tizen specific settings

--- Noble Quran/www/js/app.js
+++ Noble Quran/www/js/app.js
@@ -3,179 +3,188 @@
 angular.module('starter', ['ionic','starter.controllers', 'starter.suralist', 'starter.quran', 'ionic.utils', 'bookmark.controller' , 'settings.options'])
 
 
-.run(function($ionicPlatform, $localstorage, $rootScope, $state) {
+    .run(function($ionicPlatform, $localstorage, $rootScope, $state) {
 
-    // not a very good idea. should change later on
-    // load the settings before the view is rendered
-    // load it only once when app starts
-    $rootScope.$on('$stateChangeStart', function (event, next, current) {
-        $localstorage;
-        if($rootScope.areOptionsLoaded == undefined)
-        {
-            loadTheme();
-            loadArabicFonts();
-            loadArabicFontSize();
-            loadTranslationFontSize();
-            loadIsWordByWordEnabled();
-            loadIsTranslationEnabled();
-            loadTranslation();
-            $rootScope.areOptionsLoaded = true;
-        }
-
-
-        if(next.name != 'tab.sura-display')
-        {
-            $rootScope.hideTabs = '';
-        }
-        else
-        {
-            $rootScope.hideTabs = 'tabs-item-hide';
-        }
-    })
-
-
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-          cordova.plugins.Keyboard.disableScroll(true);
-
-        }
-        if (window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleDefault();
-        }
-    })
-
-
-    // Back button handling
-    $ionicPlatform.registerBackButtonAction(function (event) {
-        if($state.current.name=="tab.suras"){
-            navigator.app.exitApp();
-        }
-        else {
-            navigator.app.backHistory();
-        }
-    }, 100)
-
-
-    // tizen OS : back key handling
-    window.addEventListener( 'tizenhwkey', function( ev ) {
-        if( ev.keyName === "back" ) {
-            try {
-                if($state.current.name=="tab.suras"){
-                    tizen.application.getCurrentApplication().exit();
-                }
-                else {
-                    window.history.back();
-                }
-            } catch (ignore) {
+        // not a very good idea. should change later on
+        // load the settings before the view is rendered
+        // load it only once when app starts
+        $rootScope.$on('$stateChangeStart', function (event, next, current) {
+            $localstorage;
+            if($rootScope.areOptionsLoaded == undefined)
+            {
+                loadTheme();
+                loadArabicFonts();
+                loadArabicFontSize();
+                loadTranslationFontSize();
+                loadIsWordByWordEnabled();
+                loadIsTranslationEnabled();
+                loadTranslation();
+                checkIfTizenPlatform();
+                $rootScope.areOptionsLoaded = true;
             }
+
+
+            if(next.name != 'tab.sura-display')
+            {
+                $rootScope.hideTabs = '';
+            }
+            else
+            {
+                $rootScope.hideTabs = 'tabs-item-hide';
+            }
+        })
+
+
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        })
+
+
+        // Back button handling
+        $ionicPlatform.registerBackButtonAction(function (event) {
+            if($state.current.name=="tab.suras"){
+                navigator.app.exitApp();
+            }
+            else {
+                navigator.app.backHistory();
+            }
+        }, 100)
+
+
+        // tizen OS : back key handling
+        window.addEventListener( 'tizenhwkey', function( ev ) {
+            if( ev.keyName === "back" ) {
+                try {
+                    if($state.current.name=="tab.suras"){
+                        tizen.application.getCurrentApplication().exit();
+                    }
+                    else {
+                        window.history.back();
+                    }
+                } catch (ignore) {
+                }
+            }
+        })
+
+        var checkIfTizenPlatform = function() {
+            $rootScope.isTizenPlatform = false;
+            try {
+                tizen.application;
+                $rootScope.isTizenPlatform = true;
+            } catch (ignore) {}
         }
+
+
+        var loadIsTranslationEnabled = function() {
+            $rootScope.isTranslationChecked = $localstorage.getOptionTranslationEnabled();
+        }
+
+        var loadTranslation = function() {
+            $rootScope.translation = $localstorage.getOptionTranslation();
+        }
+
+        var loadIsWordByWordEnabled = function() {
+            $rootScope.isWordbyWordChecked = $localstorage.getOptionWordByWordEnabled();
+        }
+
+        var loadTheme = function() {
+            $rootScope.apptheme = $localstorage.getOptionTheme();
+        }
+
+        var loadArabicFonts = function() {
+            $rootScope.arabicFont = $localstorage.getOptionArabicFont();
+        }
+
+        var loadArabicFontSize = function() {
+            $rootScope.arabicFontSize = $localstorage.getOptionArabicFontSize();
+        }
+
+        var loadTranslationFontSize = function() {
+            $rootScope.translationFontSize = $localstorage.getOptionTranslationFontSize();
+        }
+
+
     })
 
 
-    var loadIsTranslationEnabled = function() {
-        $rootScope.isTranslationChecked = $localstorage.getOptionTranslationEnabled();
-    }
+    .config(function($stateProvider, $urlRouterProvider) {
 
-    var loadTranslation = function() {
-        $rootScope.translation = $localstorage.getOptionTranslation();
-    }
+        $stateProvider
 
-    var loadIsWordByWordEnabled = function() {
-        $rootScope.isWordbyWordChecked = $localstorage.getOptionWordByWordEnabled();
-    }
-
-    var loadTheme = function() {
-        $rootScope.apptheme = $localstorage.getOptionTheme();
-    }
-
-    var loadArabicFonts = function() {
-        $rootScope.arabicFont = $localstorage.getOptionArabicFont();
-    }
-
-    var loadArabicFontSize = function() {
-        $rootScope.arabicFontSize = $localstorage.getOptionArabicFontSize();
-    }
-
-    var loadTranslationFontSize = function() {
-        $rootScope.translationFontSize = $localstorage.getOptionTranslationFontSize();
-    }
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
 
 
-})
+            .state('tab.suras', {
+                url: '/suras',
+                views: {
+                    'tab-suras': {
+                        templateUrl: 'templates/tab-suras.html',
+                        controller: 'SurasCtrl'
+                    }
+                }
+            })
 
+            .state('tab.bookmarks', {
+                url: '/bookmarks',
+                views: {
+                    'tab-bookmarks': {
+                        templateUrl: 'templates/tab-bookmarks.html',
+                        controller: 'BookmarksCtrl'
+                    }
+                }
+            })
 
-.config(function($stateProvider, $urlRouterProvider) {
+            .state('tab.settings', {
+                url: '/settings',
+                views: {
+                    'tab-settings': {
+                        templateUrl: 'templates/tab-settings.html',
+                        controller: 'SettingsCtrl'
+                    }
+                }
+            })
 
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-  .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-  })
-
-
-  .state('tab.suras', {
-        url: '/suras',
-        views: {
-          'tab-suras': {
-            templateUrl: 'templates/tab-suras.html',
-            controller: 'SurasCtrl'
-          }
-        }
-  })
-
-  .state('tab.bookmarks', {
-      url: '/bookmarks',
-      views: {
-          'tab-bookmarks': {
-              templateUrl: 'templates/tab-bookmarks.html',
-              controller: 'BookmarksCtrl'
-          }
-      }
-  })
-
-  .state('tab.settings', {
-      url: '/settings',
-      views: {
-          'tab-settings': {
-              templateUrl: 'templates/tab-settings.html',
-              controller: 'SettingsCtrl'
-          }
-      }
-  })
-
-  .state('tab.sura-display', {
-      url: '/suras/:suraIndex',
-      views: {
-          'tab-suras': {
-              templateUrl: 'templates/sura-display.html',
-              controller: 'SuraDisplayCtrl'
-          }
-      },
-      params:{'suraIndex':null, 'ayaIndex' : null}
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/suras');
-
-})
-
-
-
-.directive('hideTabs', function($rootScope) {
-    return {
-        restrict: 'A',
-        link: function($scope, $el) {
-            $rootScope.hideTabs = 'tabs-item-hide';
-            $scope.$on('$destroy', function() {
-                $rootScope.hideTabs = '';
+            .state('tab.sura-display', {
+                url: '/suras/:suraIndex',
+                views: {
+                    'tab-suras': {
+                        templateUrl: 'templates/sura-display.html',
+                        controller: 'SuraDisplayCtrl'
+                    }
+                },
+                params:{'suraIndex':null, 'ayaIndex' : null}
             });
-        }
-    };
-});
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/suras');
+
+    })
+
+
+
+    .directive('hideTabs', function($rootScope) {
+        return {
+            restrict: 'A',
+            link: function($scope, $el) {
+                $rootScope.hideTabs = 'tabs-item-hide';
+                $scope.$on('$destroy', function() {
+                    $rootScope.hideTabs = '';
+                });
+            }
+        };
+    });
--- Noble Quran/www/js/factory/settingsFactory.js
+++ Noble Quran/www/js/factory/settingsFactory.js
@@ -1,88 +1,104 @@
 angular.module('settings.options', [])
 
 
-    .factory('$settingsOptions', ['$localstorage',function($localstorage) {
+    .factory('$settingsOptions', ['$localstorage', '$rootScope',function($localstorage, $rootScope) {
 
-        var translations = ['English - Sahih', 'English - Yusuf Ali','Malayalam', 'Urdu'];
-        var translationFiles = ['en.sahih', 'en.yusufali', 'ml.abdulhameed', 'ur.maududi'];
+            var translations = ['English - Sahih', 'English - Yusuf Ali','Malayalam', 'Urdu'];
+            var translationFiles = ['en.sahih', 'en.yusufali', 'ml.abdulhameed', 'ur.maududi'];
 
-        var themes = ['Green', 'Blue', 'White', 'Dark'];
-        var themesClasses = ['app-theme-green', 'app-theme-blue', 'app-theme-white', 'app-theme-dark'];
+            var themes = ['Green', 'Blue', 'White', 'Dark'];
+            var themesClasses = ['app-theme-green', 'app-theme-blue', 'app-theme-white', 'app-theme-dark'];
 
-        var arabicFont = ['Default', 'Qalam Majeed', 'KFGQPC Hafs', 'Me-Quran', 'NooreHuda', 'PDMS Saleem' , 'KFGQPC Uthman'];
-        var arabicFontClasses = ['arabic-font-default', 'arabic-font-qalam', 'arabic-font-hafs', 'arabic-font-mequran', 'arabic-font-noorehuda', 'arabic-font-saleem', 'arabic-font-uthman'];
+            var arabicFont = ['Default', 'Qalam Majeed', 'KFGQPC Hafs', 'Me-Quran', 'NooreHuda', 'PDMS Saleem' , 'KFGQPC Uthman'];
+            var arabicFontClasses = ['arabic-font-default', 'arabic-font-qalam', 'arabic-font-hafs', 'arabic-font-mequran', 'arabic-font-noorehuda', 'arabic-font-saleem', 'arabic-font-uthman'];
 
-        var arabicFontSize = [18, 20, 22, 24, 26, 28, 30, 32];
-        var arabicFontSizeClasses = ['arabic-font-size-18', 'arabic-font-size-20', 'arabic-font-size-22',
-                                    'arabic-font-size-24', 'arabic-font-size-26', 'arabic-font-size-28',
-                                    'arabic-font-size-30', 'arabic-font-size-32'];
+            var arabicFontSize = [18, 20, 22, 24, 26, 28, 30, 32];
+            var arabicFontSizeClasses = ['arabic-font-size-18', 'arabic-font-size-20', 'arabic-font-size-22',
+                'arabic-font-size-24', 'arabic-font-size-26', 'arabic-font-size-28',
+                'arabic-font-size-30', 'arabic-font-size-32'];
 
-        var translationFontSize = [12, 14, 16, 18];
-        var translationFontSizeClasses = ['trans-font-size-12', 'trans-font-size-14',
-                                            'trans-font-size-16', 'trans-font-size-18'];
+            var translationFontSize = [12, 14, 16, 18];
+            var translationFontSizeClasses = ['trans-font-size-12', 'trans-font-size-14',
+                'trans-font-size-16', 'trans-font-size-18'];
 
 
 
-        var getItemFromArray2CorrespondingToItemInArray1 = function(array1, array2, item)
-        {
-            for(var index=0; index<array1.length; index++)
+            var getItemFromArray2CorrespondingToItemInArray1 = function(array1, array2, item)
             {
-                if(item == array1[index])
+                for(var index=0; index<array1.length; index++)
                 {
-                    return array2[index];
+                    if(item == array1[index])
+                    {
+                        return array2[index];
+                    }
                 }
             }
-        }
 
-        return {
-            getTranslations: function() {
-                return translations;
-            },
-            getSelectedTranslation: function(selectedIndex) {
-                 return translationFiles[selectedIndex];
-            },
-            getSelectedTranslationTitle: function(translationFile) {
-                return getItemFromArray2CorrespondingToItemInArray1(translationFiles, translations, translationFile);
-            },
-            getThemes: function() {
-                 return themes;
-            },
-            getSelectedThemeClass: function(selectedIndex) {
-                return themesClasses[selectedIndex];
-            },
-            getSelectedThemeTitle: function(themeClass) {
-                return getItemFromArray2CorrespondingToItemInArray1(themesClasses, themes, themeClass);
+            return {
+                getTranslations: function() {
+                    return translations;
+                },
+                getSelectedTranslation: function(selectedIndex) {
+                    return translationFiles[selectedIndex];
+                },
+                getSelectedTranslationTitle: function(translationFile) {
+                    return getItemFromArray2CorrespondingToItemInArray1(translationFiles, translations, translationFile);
+                },
+                getThemes: function() {
+                    return themes;
+                },
+                getSelectedThemeClass: function(selectedIndex) {
+                    return themesClasses[selectedIndex];
+                },
+                getSelectedThemeTitle: function(themeClass) {
+                    return getItemFromArray2CorrespondingToItemInArray1(themesClasses, themes, themeClass);
 
-            },
-            getArabicFonts: function() {
-             return arabicFont;
-            },
-            getSelectedArabicFontClass: function(selectedIndex) {
-                return arabicFontClasses[selectedIndex];
-            },
-            getSelectedArabicFontTitle: function(font) {
-                return getItemFromArray2CorrespondingToItemInArray1(arabicFontClasses, arabicFont, font);
+                },
+                getArabicFonts: function() {
+                    var availableFonts = arabicFont;
+                    if($rootScope.isTizenPlatform) {
+                        availableFonts = ['Default', 'Qalam Majeed', 'PDMS Saleem'];
+                    }
+                    return availableFonts;
+                },
+                getSelectedArabicFontClass: function(selectedIndex) {
+                    var availableFontsClass = arabicFontClasses;
+                    if($rootScope.isTizenPlatform) {
+                        availableFontsClass = ['arabic-font-default', 'arabic-font-qalam', 'arabic-font-saleem'];
+                    }
+                    return availableFontsClass[selectedIndex];
+                },
+                getSelectedArabicFontTitle: function(font) {
+                    return getItemFromArray2CorrespondingToItemInArray1(arabicFontClasses, arabicFont, font);
 
-            },
-            getArabicFontSizes: function() {
-             return arabicFontSize;
-            },
-            getSelectedArabicFontSizeClass: function(selectedIndex) {
-                return arabicFontSizeClasses[selectedIndex];
-            },
-            getSelectedArabicFontSizeTitle: function(fontSize) {
-                return getItemFromArray2CorrespondingToItemInArray1(arabicFontSizeClasses, arabicFontSize, fontSize);
+                },
+                getArabicFontSizes: function() {
+                    var availableFontSizes = arabicFontSize;
+                    if($rootScope.isTizenPlatform) {
+                        availableFontSizes = [24, 28, 30, 32];
+                    }
+                    return availableFontSizes;
+                },
+                getSelectedArabicFontSizeClass: function(selectedIndex) {
+                    var availableFontSizeClasses = arabicFontSizeClasses;
+                    if($rootScope.isTizenPlatform) {
+                        availableFontSizeClasses = ['arabic-font-size-24', 'arabic-font-size-28','arabic-font-size-30', 'arabic-font-size-32'];
+                    }
+                    return availableFontSizeClasses[selectedIndex];
+                },
+                getSelectedArabicFontSizeTitle: function(fontSize) {
+                    return getItemFromArray2CorrespondingToItemInArray1(arabicFontSizeClasses, arabicFontSize, fontSize);
 
-            },
-            getTranslationFontSizes: function() {
-             return translationFontSize;
-            },
-            getSelectedTranslationFontSizeClass: function(selectedIndex) {
-                 return translationFontSizeClasses[selectedIndex];
-            },
-            getSelectedTranslationFontSizeTitle: function(fontSize) {
-                return getItemFromArray2CorrespondingToItemInArray1(translationFontSizeClasses, translationFontSize, fontSize);
+                },
+                getTranslationFontSizes: function() {
+                    return translationFontSize;
+                },
+                getSelectedTranslationFontSizeClass: function(selectedIndex) {
+                    return translationFontSizeClasses[selectedIndex];
+                },
+                getSelectedTranslationFontSizeTitle: function(fontSize) {
+                    return getItemFromArray2CorrespondingToItemInArray1(translationFontSizeClasses, translationFontSize, fontSize);
 
-            }
+                }
     }
 }]);
